750f763247a140a12330a40f8917b26a3bc45902
aasimmd <aasim.md00@gmail.com>
2021-02-27 10:41:51 +0530

    basic struct

---
+++ client.c
@@ -0,0 +1,1 @@
+#include "grocery.h"
---
+++ grocery.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct customers
+{
+    int id;
+    char password[10];
+    int w_money;
+} customer;
+
+typedef struct admins
+{
+    int id;
+    char password[10];
+} admin;
