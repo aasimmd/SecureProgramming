a817abadbbf3d4eecd0346efba67070f309b6b27
aasimmd <aasim.md00@gmail.com>
2021-03-27 16:41:01 +0530

    basic main function

--- client.c
+++ client.c
@@ -1,1 +1,43 @@
 #include "grocery.h"
+
+int main()
+{
+    printf("Enter your choice : \n");
+    printf("1. Login\n");
+    printf("2. Register\n");
+    int lr;
+    scanf("%d",&lr);
+    switch (lr)
+    {
+        case 1:
+            printf("Enter choice : \n");
+            printf("1. Customer\n");
+            printf("2. Admin\n");
+            int ac;
+            scanf("%d", &ac);
+            switch (ac)
+            {
+                case 1:
+                    int cchk = checkcustomerlogin();
+                    break;
+                
+                case 2:
+                    int achk = checkadminlogin();
+                    break;
+
+                default:
+                    printf("Invalid option");
+                    break;
+            }
+            break;
+
+        case 2:
+            registerCustomer();
+            break;
+        
+        default:
+            printf("Invalid Option");
+            break;
+    }
+    return 0;
+}
